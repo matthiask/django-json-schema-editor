0740ae671bfad052a109128860e78c76568b25ed
Matthias Kestenholz <mk@feinheit.ch>
2021-12-20 12:17:01 +0100

    Is there a reason for NOT prettifying the rest of webapp/static?

--- jsoneditorwidget/static/jsoneditorwidget/widget.js
+++ jsoneditorwidget/static/jsoneditorwidget/widget.js
@@ -1,60 +1,63 @@
-document.addEventListener("DOMContentLoaded", event => {
-  document.querySelectorAll(".inline-related:not(.empty-form) .jsoneditorwidget").forEach(el => {
-    DjangoJSONEditorWidget.initWidget(el);
-  });
+/* global django, JSONEditor */
+document.addEventListener("DOMContentLoaded", () => {
+  document
+    .querySelectorAll(".inline-related:not(.empty-form) .jsoneditorwidget")
+    .forEach((el) => {
+      DjangoJSONEditorWidget.initWidget(el)
+    })
 })
 
-document.addEventListener("DOMContentLoaded", event => {
-  django.jQuery(document).on("formset:added", (event, $row, formsetName) => {
-    $row.get().forEach(el => {
-      el.querySelectorAll(".jsoneditorwidget").forEach(
-        el => DjangoJSONEditorWidget.initWidget(el)
+document.addEventListener("DOMContentLoaded", () => {
+  django.jQuery(document).on("formset:added", (event, $row, _formsetName) => {
+    $row.get().forEach((el) => {
+      el.querySelectorAll(".jsoneditorwidget").forEach((el) =>
+        DjangoJSONEditorWidget.initWidget(el)
       )
-    });
-  });
+    })
+  })
 })
 
 const DjangoJSONEditorWidget = {
   initWidget: (el) => {
-    DjangoJSONEditorWidget.initEditor(el);
-    DjangoJSONEditorWidget.initDatafieldToggle(el);
+    DjangoJSONEditorWidget.initEditor(el)
+    DjangoJSONEditorWidget.initDatafieldToggle(el)
   },
   initEditor: (el) => {
     const container = el.querySelector(".editor")
     const input = el.querySelector("textarea")
     const config = JSON.parse(el.dataset["editorConfig"])
-    const editor = new JSONEditor(container, config);
+    const editor = new JSONEditor(container, config)
 
-    const data = input.value ? JSON.parse(input.value) : null;
+    const data = input.value ? JSON.parse(input.value) : null
     if (data) {
       editor.setValue(data)
     }
 
-    editor.on('change', e => {
-      input.value = JSON.stringify(editor.getValue());
-    });
+    editor.on("change", () => {
+      input.value = JSON.stringify(editor.getValue())
+    })
   },
   initDatafieldToggle: (el) => {
-    const toggle = el.querySelector('.datafield-toggle');
-    const showText = toggle.dataset["showText"];
-    const hideText = toggle.dataset["hideText"];
-    const wrapper = el.querySelector(".datafield-wrapper");
+    const toggle = el.querySelector(".datafield-toggle")
+    const showText = toggle.dataset["showText"]
+    const hideText = toggle.dataset["hideText"]
+    const wrapper = el.querySelector(".datafield-wrapper")
 
-    toggle.text = showText;
+    toggle.text = showText
     wrapper.classList.add("hidden")
 
-    toggle.addEventListener("click", e => {
-      e.preventDefault();
+    toggle.addEventListener("click", (e) => {
+      e.preventDefault()
 
       if (wrapper.classList.contains("hidden")) {
         // show
-        wrapper.classList.remove("hidden");
-        toggle.text = hideText;
+        wrapper.classList.remove("hidden")
+        toggle.text = hideText
       } else {
         // hide
-        wrapper.classList.add("hidden");
-        toggle.text = showText;
+        wrapper.classList.add("hidden")
+        toggle.text = showText
       }
     })
-  }
+  },
 }
