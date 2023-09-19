ff2de1d79af8bf973fb59f339c6ed092cb8dd725
Matthias Kestenholz <mk@feinheit.ch>
2023-09-19 14:25:50 +0200

    This was probably a bug

--- jsoneditorwidget/__init__.py
+++ jsoneditorwidget/__init__.py
@@ -24,7 +24,7 @@ class LazyJSONEncoder(json.JSONEncoder):
         ):
             return force_str(obj)
 
-        super().default(obj)
+        return super().default(obj)
 
 
 class JSONEditorWidget(forms.Textarea):
