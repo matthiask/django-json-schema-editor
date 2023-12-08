09671b3f7e3ae65d2c1221ff282781c860c46e29
Matthias Kestenholz <mk@feinheit.ch>
2023-12-08 21:20:22 +0100

    Streamline the JS a bit

--- django_json_schema_editor/static/django_json_schema_editor/widget.js
+++ django_json_schema_editor/static/django_json_schema_editor/widget.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", () => {
       ".inline-related:not(.empty-form) .django_json_schema_editor",
     )
     .forEach((el) => {
-      DjangoJSONEditorWidget.initWidget(el)
+      initEditor(el)
     })
 })
 
@@ -13,29 +13,27 @@ document.addEventListener("DOMContentLoaded", () => {
   django.jQuery(document).on("formset:added", (event) => {
     event.target
       .querySelectorAll(".django_json_schema_editor")
-      .forEach((el) => DjangoJSONEditorWidget.initWidget(el))
+      .forEach((el) => {
+        initEditor(el)
+      })
   })
 })
 
-const DjangoJSONEditorWidget = {
-  initWidget: (el) => {
-    DjangoJSONEditorWidget.initEditor(el)
-  },
-  initEditor: (el) => {
-    const container = el.querySelector(".editor")
-    const input = el.querySelector("textarea")
-    const config = JSON.parse(el.dataset["editorConfig"])
-    const editor = new JSONEditor(container, config)
+const initEditor = (el) => {
+  const container = el.querySelector(".editor")
+  const input = el.querySelector("textarea")
+  const config = JSON.parse(el.dataset.editorConfig)
+  console.debug(container, config)
+  const editor = new JSONEditor(container, config)
 
-    editor.on("ready", () => {
-      const data = input.value ? JSON.parse(input.value) : null
-      if (data) {
-        editor.setValue(data)
-      }
-    })
+  editor.on("ready", () => {
+    const data = input.value ? JSON.parse(input.value) : null
+    if (data) {
+      editor.setValue(data)
+    }
+  })
 
-    editor.on("change", () => {
-      input.value = JSON.stringify(editor.getValue())
-    })
-  },
+  editor.on("change", () => {
+    input.value = JSON.stringify(editor.getValue())
+  })
 }
