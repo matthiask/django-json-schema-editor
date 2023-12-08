e3954320a335518d357963a3563be0571ce2b1e8
Matthias Kestenholz <mk@feinheit.ch>
2023-12-08 18:25:52 +0100

    Make string fields take up more space

--- jsoneditorwidget/fields.py
+++ jsoneditorwidget/fields.py
@@ -7,12 +7,11 @@ from jsoneditorwidget.forms import JSONEditorField
 
 class JSONField(models.JSONField):
     def __init__(self, *args, **kwargs):
-        self._schema = kwargs.pop("schema")
+        self._schema = kwargs.pop("schema", None)
         super().__init__(*args, **kwargs)
 
     def deconstruct(self):
         name, path, args, kwargs = super().deconstruct()
-        kwargs["choices"] = [("", "")]
         return name, "django.db.models.JSONField", args, kwargs
 
     def formfield(self, **kwargs):
